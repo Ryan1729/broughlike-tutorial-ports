ec1c8965216a1aa34580abd90b4f100a9d617000
Ryan1729 <ryan1729@gmail.com>
2020-06-23 01:06:00 -0600

    add instructive error

--- js/game.js
+++ js/game.js
@@ -2,4 +2,9 @@
 function setupCanvas(){
     canvas = document.querySelector("canvas");
     ctx = canvas.getContext("2d");
+
+    canvas.width = tileSize*(numTiles+uiWidth);
+    canvas.height = tileSize*numTiles;
+    canvas.style.width = canvas.width + 'px';
+    canvas.style.height = canvas.height + 'px';
 }
