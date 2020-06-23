e79e8caaa5800649b7254b350296ba14f740bcb0
Ryan1729 <ryan1729@gmail.com>
2020-06-23 01:06:00 -0600

    add `getTile`

--- js/map.js
+++ js/map.js
@@ -15,3 +15,15 @@ function generateTiles(){
         }
     }
 }
+
+function inBounds(x,y){
+    return x>0 && y>0 && x<numTiles-1 && y<numTiles-1;
+}
+
+function getTile(x, y){
+    if(inBounds(x,y)){
+        return tiles[x][y];
+    }else{
+        return new Wall(x,y);
+    }
+}
