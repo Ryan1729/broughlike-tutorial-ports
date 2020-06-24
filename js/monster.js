c43a27c216c6fc567fb76e9b31fbfc61acf807a2
Ryan1729 <Ryan1729@gmail.com>
2020-06-24 01:06:00 -0600

    implement Jester abilities

--- js/monster.js
+++ js/monster.js
@@ -154,4 +154,11 @@ class Jester extends Monster{
     constructor(tile){
         super(tile, 8, 2);
     }
+
+    doStuff(){
+        let neighbors = this.tile.getAdjacentPassableNeighbors();
+        if(neighbors.length){
+            this.tryMove(neighbors[0].x - this.tile.x, neighbors[0].y - this.tile.y);
+        }
+    }
 }
