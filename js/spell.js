252456cff5196a4b2f67a8f37e07ce4231461920
Ryan1729 <Ryan1729@gmail.com>
2020-06-28 01:06:00 -0600

    add QUAKE

--- js/spell.js
+++ js/spell.js
@@ -2,5 +2,17 @@
 spells = {
     WOOP: function(){
         player.move(randomPassableTile());
+    },
+    QUAKE: function(){
+        for(let i=0; i<numTiles; i++){
+            for(let j=0; j<numTiles; j++){
+                let tile = getTile(i,j);
+                if(tile.monster){
+                    let numWalls = 4 - tile.getAdjacentPassableNeighbors().length;
+                    tile.monster.hit(numWalls*2);
+                }
+            }
+        }
+        shakeAmount = 20;
     }
 };
