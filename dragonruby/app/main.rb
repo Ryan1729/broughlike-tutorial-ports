c8d7b902b2ef3f06880856f1e64a1a4fefde16c3
Ryan1729 <Ryan1729@gmail.com>
2020-07-21 01:06:00 -0600

    set background colour and add border, along with constants that define the border size

--- dragonruby/app/main.rb
+++ dragonruby/app/main.rb
@@ -11,17 +11,40 @@ def tick args
   draw(args)
 end
 
+TileSize = 80;
+NumTiles = 9;
+UIWidth = 4;
+
 def draw args
   s = args.state
+
+  # copy the screen size and ake it slighlty bigger to fix the gray 
+  # peeking through when fullscreened
+  background = args.grid.rect.scale_rect(1.1)
+  # add on the colour we want
+  background += [75, 0, 130]
+  args.outputs.solids << background
+  
+  # the -1 and +2 business makes the border lie just outside the actual 
+  # play area
+  args.outputs.borders << [
+    (1.5 * TileSize) - 1,
+    -1,
+    (NumTiles + UIWidth) * TileSize + 2,
+    NumTiles * TileSize + 2,
+    255,
+    255,
+    255
+  ]
 
   args.outputs.sprites << {
     x: s.x,
     y: s.y,
-    w: 128,
-    h: 128,
+    w: TileSize,
+    h: TileSize,
     path: "sprites/spritesheet.png",
     source_x: 0,
     source_y: 0,
     source_w: 16,
-  }  
+  }
 end
