6e2e042cf729b13aae2bee626faa50f64ce1c55b
Ryan1729 <Ryan1729@gmail.com>
2020-07-21 01:06:00 -0600

    add basic player movement

--- dragonruby/app/main.rb
+++ dragonruby/app/main.rb
@@ -1,24 +1,27 @@
 def tick args
-  args.outputs.labels << [200, 200, 'Hello World!', 50]
+  s = args.state
+  s.x ||= 0
+  s.y ||= 0
+  key_down = args.inputs.keyboard.key_down
+  if key_down.w then s.y += 8 end
+  if key_down.s then s.y -= 8 end
+  if key_down.a then s.x -= 8 end
+  if key_down.d then s.x += 8 end
+
+  draw(args)
+end
+
+def draw args
+  s = args.state
+
   args.outputs.sprites << {
-    x: 100,
-    y: 100,
-    w: 100,
-    h: 100,
+    x: s.x,
+    y: s.y,
+    w: 128,
+    h: 128,
     path: "sprites/spritesheet.png",
     source_x: 0,
     source_y: 0,
     source_w: 16,
-  }
-  
-  args.outputs.sprites << {
-    x: 300,
-    y: 400,
-    w: 400,
-    h: 400,
-    path: "sprites/spritesheet.png",
-    source_x: 16,
-    source_y: 0,
-    source_w: 16,
-  }
+  }  
 end
