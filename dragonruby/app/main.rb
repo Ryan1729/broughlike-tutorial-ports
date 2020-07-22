f565ec463939a89f94029f25e8367986b52581fd
Ryan1729 <Ryan1729@gmail.com>
2020-07-22 01:06:00 -0600

    add drawSprite function and handle play area offsetting inside it

--- dragonruby/app/main.rb
+++ dragonruby/app/main.rb
@@ -10,10 +10,10 @@ def tick args
   s.x ||= 0
   s.y ||= 0
   key_down = args.inputs.keyboard.key_down
-  if key_down.w then s.y += 8 end
-  if key_down.s then s.y -= 8 end
-  if key_down.a then s.x -= 8 end
-  if key_down.d then s.x += 8 end
+  if key_down.w then s.y += 1 end
+  if key_down.s then s.y -= 1 end
+  if key_down.a then s.x -= 1 end
+  if key_down.d then s.x += 1 end
 
   draw args
 end
@@ -21,6 +21,10 @@ end
 TileSize = 80;
 NumTiles = 9;
 UIWidth = 4;
+PlayAreaMinX = 1.5 * TileSize
+PlayAreaMinY = 0
+PlayAreaMaxX = (NumTiles + UIWidth) * TileSize
+PlayAreaMaxY = NumTiles * TileSize
 
 def draw args
   s = args.state
@@ -30,25 +34,27 @@ def draw args
   # the -1 and +2 business makes the border lie just outside the actual 
   # play area
   args.outputs.borders << [
-    (1.5 * TileSize) - 1,
-    -1,
-    (NumTiles + UIWidth) * TileSize + 2,
-    NumTiles * TileSize + 2,
+    PlayAreaMinX - 1,
+    PlayAreaMinY - 1,
+    PlayAreaMaxX + 2,
+    PlayAreaMaxY + 2,
     255,
     255,
     255
   ]
 
+  drawSprite args, 0, s.x, s.y
+end
+
+def drawSprite args, sprite, x, y
   args.outputs.sprites << {
-    x: s.x,
-    y: s.y,
+    x: PlayAreaMinX + x * TileSize,
+    y: PlayAreaMinY + y * TileSize,
     w: TileSize,
     h: TileSize,
     path: "sprites/spritesheet.png",
-    source_x: 0,
+    source_x: sprite * 16,
     source_y: 0,
     source_w: 16,
   }
-  
-  
 end
