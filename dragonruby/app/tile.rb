b862ec02434d506c957fc64502f2f07d02d4b577
Ryan1729 <Ryan1729@gmail.com>
2020-07-23 01:06:00 -0600

    add serialization to Tile so that dragonruby doesn't log so much if we make a mistake

--- dragonruby/app/tile.rb
+++ dragonruby/app/tile.rb
@@ -11,6 +11,29 @@ class Tile
   def draw args
     drawSprite args, sprite, x, y
   end
+  
+  ## Dragonruby output these instructions to enable serialization on our
+  ## class, so we complied.
+  # 1. Create a serialize method that returns a hash with all of
+  #    the values you care about.
+  def serialize
+    { 
+      x => x,
+      y => y,
+      sprite => sprite,
+      passable => passable
+    }
+  end
+
+  # 2. Override the inspect method and return `serialize.to_s`.
+  def inspect
+    serialize.to_s
+  end
+
+  # 3. Override to_s and return `serialize.to_s`.
+  def to_s
+    serialize.to_s
+  end
 end
 
 class Floor < Tile
