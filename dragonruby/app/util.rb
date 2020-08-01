5315409295ce678d13319e3cd4d707bdad40b380
Ryan1729 <Ryan1729@gmail.com>
2020-08-01 01:06:00 -0600

    address non-local return lint

--- dragonruby/app/util.rb
+++ dragonruby/app/util.rb
@@ -1,8 +1,8 @@
 def tryTo(description, callback)
-  (0..1000).reverse_each do |_timeout|
-    return if callback.call
+  throw 'Timeout while trying to ' + description unless
+  (0..1000).any? do |_timeout|
+    callback.call
   end
-  throw 'Timeout while trying to ' + description
 end
 
 def randomRange(min, max)
