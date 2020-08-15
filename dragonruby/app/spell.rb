0017d65f97e5f83e290514d1fad8314ff96a1044
Ryan1729 <Ryan1729@gmail.com>
2020-08-15 01:06:00 -0600

    add quake

--- dragonruby/app/spell.rb
+++ dragonruby/app/spell.rb
@@ -3,5 +3,17 @@
 Spells = {
   WOOP: lambda {|s|
     s.player.move s, s.tiles.randomPassable
+  },
+  QUAKE: lambda {|s|
+    (0...NumTiles).each do |i|
+      (0...NumTiles).each do |j|
+        tile = s.tiles.get i, j
+        if tile.monster
+          numWalls = 4 - tile.getAdjacentPassableNeighbors(s.tiles).length
+          tile.monster.hit(s, numWalls * 2)
+        end
+      end
+    end
+    s.shakeAmount = 20
   }
 }.freeze
