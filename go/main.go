841ea35bbf8c08160c3ed72ec793272c5eef6067
Ryan1729 <Ryan1729@gmail.com>
2020-08-22 01:06:00 -0600

    add build script that runs golangci-lint with almost everything enabled, and fix lint errors

--- go/main.go
+++ go/main.go
@@ -18,29 +18,39 @@ func main() {
 	if err != nil {
 		die(err)
 	}
-	defer window.Destroy()
+	defer func() { dieIfErr(window.Destroy()) }()
 
 	surface, err := window.GetSurface()
 	if err != nil {
 		die(err)
 	}
-	surface.FillRect(nil, 0xffffffff)
+	dieIfErr(surface.FillRect(nil, 0xffffffff))
 
-	rect := sdl.Rect{0, 0, 20, 20}
-	surface.FillRect(&rect, 0xff000000)
-	window.UpdateSurface()
+	const size = 20
+	rect := sdl.Rect{X: 0, Y: 0, W: size, H: size}
+	dieIfErr(surface.FillRect(&rect, 0xff000000))
+	dieIfErr(window.UpdateSurface())
 
 	for {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
+			if _, isQuit := event.(*sdl.QuitEvent); isQuit {
 				return
 			}
 		}
 	}
 }
 
+func dieIfErr(err error) {
+	if err != nil {
+		die(err)
+	}
+}
+
 func die(err error) {
-	sdl.ShowSimpleMessageBox(sdl.MESSAGEBOX_ERROR, "Error", err.Error(), nil)
-	panic(err)
+	msgErr := sdl.ShowSimpleMessageBox(sdl.MESSAGEBOX_ERROR, "Error", err.Error(), nil)
+	if msgErr == nil {
+		panic(err)
+	} else {
+		panic(msgErr.Error() + "\n" + err.Error())
+	}
 }
