5b3d8dc4f9a02ac71deaa2689956254f077417db
Ryan1729 <Ryan1729@gmail.com>
2020-08-23 01:06:00 -0600

    add moving player at 60 fps

--- go/main.go
+++ go/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"time"
+
 	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	PerFrameDuration = time.Second / 60
+	size             = 20
 )
 
 func main() {
@@ -19,19 +26,48 @@ func main() {
 
 	surface, err := window.GetSurface()
 	dieIfErr(err)
+
+	var x int32 = 0
+	var y int32 = 0
+
 	dieIfErr(surface.FillRect(nil, 0xffffffff))
 
-	const size = 20
-	rect := sdl.Rect{X: 0, Y: 0, W: size, H: size}
-	dieIfErr(surface.FillRect(&rect, 0xff000000))
-	dieIfErr(window.UpdateSurface())
+	draw := func() {
+		dieIfErr(surface.FillRect(&sdl.Rect{X: x * size, Y: y * size, W: size, H: size}, 0xff000000))
+		dieIfErr(window.UpdateSurface())
+	}
 
 	for {
+		start := time.Now()
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			if _, isQuit := event.(*sdl.QuitEvent); isQuit {
+			switch e := event.(type) {
+			case *sdl.QuitEvent:
 				return
+			case *sdl.KeyboardEvent:
+				switch e.Keysym.Sym {
+				case sdl.K_w:
+					fallthrough
+				case sdl.K_UP:
+					y--
+				case sdl.K_a:
+					fallthrough
+				case sdl.K_LEFT:
+					x--
+				case sdl.K_s:
+					fallthrough
+				case sdl.K_DOWN:
+					y++
+				case sdl.K_d:
+					fallthrough
+				case sdl.K_RIGHT:
+					x++
+				}
 			}
 		}
+
+		draw()
+
+		time.Sleep(time.Until(start.Add(PerFrameDuration)))
 	}
 }
 
