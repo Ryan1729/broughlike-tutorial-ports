9eda0c42b1d8745a1030d1eabb721a9e4d05f0c1
Ryan1729 <Ryan1729@gmail.com>
2020-08-25 01:06:00 -0600

    add small pack program and create assets.go with it

---
+++ go/pack/pack.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var output strings.Builder
+	// As of this writing the assets take up around 5 * 90k bytes on disk.
+	// After converting to source code, ech byte will be around 5 more bytes on
+	// average, ("128, ").
+	output.Grow(5 * 90_000 * 5)
+
+	output.Write([]byte("package assets\n\n"))
+
+	appendFileAsByteArray(Spec{output: &output, fileName: "../assets/spritesheet.png", arrayName: "Spritesheet"})
+
+	appendFileAsByteArray(Spec{output: &output, fileName: "../assets/hit1.wav", arrayName: "Hit1"})
+	appendFileAsByteArray(Spec{output: &output, fileName: "../assets/hit2.wav", arrayName: "Hit2"})
+	appendFileAsByteArray(Spec{output: &output, fileName: "../assets/newLevel.wav", arrayName: "NewLevel"})
+	appendFileAsByteArray(Spec{output: &output, fileName: "../assets/spell.wav", arrayName: "Spell"})
+	appendFileAsByteArray(Spec{output: &output, fileName: "../assets/treasure.wav", arrayName: "Treasure"})
+
+	err := ioutil.WriteFile("../assets/assets.go", []byte(output.String()), 0600)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Spec struct {
+	output    *strings.Builder
+	fileName  string
+	arrayName string
+}
+
+func appendFileAsByteArray(spec Spec) {
+	output := spec.output
+	fileData, err := ioutil.ReadFile(spec.fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	output.Write([]byte("var "))
+	output.Write([]byte(spec.arrayName))
+	output.Write([]byte(" = []byte{"))
+
+	sep := ""
+
+	for _, v := range fileData {
+		output.Write([]byte(sep))
+		output.Write([]byte(strconv.Itoa(int(v))))
+		sep = ", "
+	}
+
+	output.Write([]byte("}\n"))
+}
