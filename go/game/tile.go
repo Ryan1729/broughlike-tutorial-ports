6ef7de1f3c3b54ff45ca21d2349a4cd6c05b6379
Ryan1729 <Ryan1729@gmail.com>
2020-08-27 01:06:00 -0600

    add Tile, Floor and Wall types

--- go/game/tile.go
+++ go/game/tile.go
@@ -1,1 +1,39 @@
 package game
+
+type Tile struct {
+	x, y     Position
+	sprite   SpriteIndex
+	passable bool
+}
+
+func NewTile(sprite SpriteIndex, x, y Position, passable bool) Tile {
+	return Tile{
+		x, y,
+		sprite,
+		passable,
+	}
+}
+
+func (t *Tile) draw(p Platform) {
+	p.Sprite(t.sprite, t.x, t.y)
+}
+
+type Floor struct {
+	Tile
+}
+
+func NewFloor(x, y Position) Floor {
+	return Floor{
+		Tile: NewTile(2, x, y, true),
+	}
+}
+
+type Wall struct {
+	Tile
+}
+
+func NewWall(x, y Position) Wall {
+	return Wall{
+		Tile: NewTile(3, x, y, false),
+	}
+}
