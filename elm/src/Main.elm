1b4f7783336853cec09633c50b9004e2ae63751d
Ryan1729 <Ryan1729@gmail.com>
2020-10-05 01:06:00 -0600

    update X/Y on keyDown and switch to `requestAnimationFrame`

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,10 +1,11 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Events
 import Html
+import Json.Decode as JD
 import Ports
-import Time
-import Types exposing (Model, Msg(..))
+import Types exposing (Model, X, Y, decX, decY, incX, incY)
 
 
 main =
@@ -21,14 +22,84 @@ init _ =
     ( Types.defaultModel, Cmd.none )
 
 
-subscriptions _ =
-    Time.every 15 (\_ -> Tick)
-
-
 update msg model =
     case msg of
         Tick ->
             ( model, Ports.draw model )
+
+        Input input ->
+            ( case input of
+                Up ->
+                    { model | y = decY model.y }
+
+                Down ->
+                    { model | y = incY model.y }
+
+                Left ->
+                    { model | x = decX model.x }
+
+                Right ->
+                    { model | x = incX model.x }
+
+                Other ->
+                    model
+            , Cmd.none
+            )
+
+
+subscriptions _ =
+    Sub.batch
+        [ Browser.Events.onAnimationFrame (\_ -> Tick)
+        , JD.field "key" JD.string
+            |> JD.map toInput
+            |> Browser.Events.onKeyDown
+        ]
+
+
+type Msg
+    = Tick
+    | Input Input
+
+
+type Input
+    = Other
+    | Up
+    | Down
+    | Left
+    | Right
+
+
+toInput : String -> Msg
+toInput s =
+    Input
+        (case s of
+            "ArrowUp" ->
+                Up
+
+            "w" ->
+                Up
+
+            "ArrowDown" ->
+                Down
+
+            "s" ->
+                Down
+
+            "ArrowLeft" ->
+                Left
+
+            "a" ->
+                Left
+
+            "ArrowRight" ->
+                Right
+
+            "d" ->
+                Right
+
+            _ ->
+                Other
+        )
 
 
 view model =
--- elm/src/Types.elm
+++ elm/src/Types.elm
@@ -18,9 +18,37 @@ type X
     = X Float
 
 
+incX xx =
+    X
+        (case xx of
+            X x ->
+                x + 1
+        )
+
+
+decX xx =
+    X
+        (case xx of
+            X x ->
+                x - 1
+        )
+
+
 type Y
     = Y Float
 
 
-type Msg
-    = Tick
+incY yy =
+    Y
+        (case yy of
+            Y y ->
+                y + 1
+        )
+
+
+decY yy =
+    Y
+        (case yy of
+            Y y ->
+                y - 1
+        )
