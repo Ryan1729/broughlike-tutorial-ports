aee3954c3b235dee9c7f30e1f2f66d768abd6c27
Ryan1729 <Ryan1729@gmail.com>
2020-11-25 00:06:00 -0700

    allow moving the square, and for extra credit, add `no_ex` macro

--- nim/broughlike_tutorial.nim
+++ nim/broughlike_tutorial.nim
@@ -1,9 +1,31 @@
-import raylib
+import macros, raylib
+
+# no_ex: allow no exceptions
+macro no_ex(x: untyped): untyped =
+    #echo "x = ", tree_repr(x)
+
+    for child in x:
+        let raisesPragma: NimNode = nnkExprColonExpr.newTree(
+            newIdentNode("raises"),
+            nnkBracket.newTree()
+        )
+
+        addPragma(child, raisesPragma)
+
+    result = x
+
 
 InitWindow 0, 0, "AWESOME BROUGHLIKE"
 
 var screenWidth = GetScreenWidth()
 var screenHeight = GetScreenHeight()
+
+var x, y = 0
+
+no_ex:
+    proc draw() =
+        ClearBackground DARKGRAY
+        DrawRectangle(x*20, y*20, 20, 20, BLACK)
 
 while not WindowShouldClose():
     if IsKeyPressed(KEY_F11):
@@ -11,10 +33,17 @@ while not WindowShouldClose():
         screenWidth = GetScreenWidth()
         screenHeight = GetScreenHeight()
 
+    if IsKeyPressed(KEY_W) or IsKeyPressed(KEY_UP):
+        y -= 1
+    if IsKeyPressed(KEY_S) or IsKeyPressed(KEY_DOWN):
+        y += 1
+    if IsKeyPressed(KEY_A) or IsKeyPressed(KEY_LEFT):
+        x -= 1
+    if IsKeyPressed(KEY_D) or IsKeyPressed(KEY_RIGHT):
+        x += 1
+
     BeginDrawing()
 
-    ClearBackground DARKGRAY
-
-    DrawRectangle(0, 0, 20, 20, BLACK)
+    draw()
 
     EndDrawing()
