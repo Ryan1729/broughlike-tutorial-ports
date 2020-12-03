c4b7870f6e9a51c04dfe5354070a803de5ca6843
Ryan1729 <Ryan1729@gmail.com>
2020-12-03 00:06:00 -0700

    add outer wall

--- nim/map.nim
+++ nim/map.nim
@@ -1,6 +1,6 @@
 from random import rand
 
-from game import no_ex
+from game import no_ex, `<`, `<=`
 from tile import nil
 
 const tileLen: int = game.NumTiles * game.NumTiles
@@ -15,19 +15,35 @@ no_ex:
         for t in tiles.tiles:
             tile.draw(t, platform)
 
+    func xyToI(xy: game.TileXY): int =
+        int(xy.y) * game.NumTiles + int(xy.x)
+
+    func inBounds(xy: game.TileXY): bool =
+        let
+            x = xy.x
+            y = xy.y
+        x > 0 and y > 0 and x < game.NumTiles - 1 and y < game.NumTiles - 1
+
     proc generateTiles*(rng: var random.Rand): Tiles =
         var tiles: TilesArray
         for y in 0..<game.NumTiles:
             for x in 0..<game.NumTiles:
                 let
-                    i = y * game.NumTiles + x
                     xy = game.TileXY(x: game.TileX(x), y: game.TileY(y))
+                    i = xyToI(xy)
 
                 tiles[i] = tile.newFloor(xy)
                 
-                if rand(rng, 1.0) < 0.3:
+                if (not inBounds(xy)) or rand(rng, 1.0) < 0.3:
                     tiles[i] = tile.newWall(xy)
                 else:
                     tiles[i] = tile.newFloor(xy)
                 
-        result = Tiles(tiles: tiles)
+        Tiles(tiles: tiles)
+
+    func getTile*(tiles: Tiles, xy: game.TileXY): tile.Tile =
+        if inBounds xy:
+            tiles.tiles[xyToI(xy)]
+        else:
+            tile.newWall(xy)
+
