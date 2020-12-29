31f4ee859ab9aafaadcda1fc1991620e07e50a5b
Ryan1729 <Ryan1729@gmail.com>
2020-12-29 00:06:00 -0700

    Revert "attempt to load a font and run into weird SIGSEGV violations that I am not interested in debugging"
    This reverts commit 4c61c7c44ce322dac005259aaaa2e4ae309919c9.

--- nim/assets.nim
+++ nim/assets.nim
@@ -3,12 +3,3 @@ import raylib
 from spritesheet import nil
 
 var spritesheetImage*: Image = spritesheet.image
-echo "from font import nil"
-
-from font import nil
-#[
-echo "loadedFont"
-var loadedFont*: Font = font.loadedFont
-echo "fontLicense"
-var fontLicense*: string = font.license
-]#
--- nim/pack/pack.nim
+++ nim/pack/pack.nim
@@ -1,30 +1,27 @@
 import strutils, system
 
-# spritesheet.nim
-# {
-block:
-    let image = readFile("../spritesheet.png")
-    let imageLen: int = len(image)
+let image = readFile("../spritesheet.png")
+let imageLen: int = len(image)
 
-    var output = """
+var output = """
 import raylib
 
 var imageArray: array["""
-    add(output, $imageLen)
-    add(output, ", char] = [")
+add(output, $imageLen)
+add(output, ", char] = [")
 
-    let startLen = len(output)
+let startLen = len(output)
 
-    for c in image:
-        addSep(output, startLen = startLen)
-        add(output, "'\\x")
-        add(output, toHex(uint8(c)))
-        add(output, "'")
-        
+for c in image:
+    addSep(output, startLen = startLen)
+    add(output, "'\\x")
+    add(output, toHex(uint8(c)))
+    add(output, "'")
+    
 
-    add(output, "]\n")
+add(output, "]\n")
 
-    add(output, """
+add(output, """
 
 var imageBytes: ptr char = cast[ptr char](addr(imageArray))
 
@@ -32,57 +29,7 @@ var image*: Image = LoadImageFromMemory(
     "png",
     imageBytes,
     """)
-    add(output, $imageLen)
-    add(output, "\n)\n")
+add(output, $imageLen)
+add(output, "\n)\n")
 
-    writeFile("../spritesheet.nim", output)
-# }
-
-
-# font.nim
-# {
-block:
-    let font = readFile("../fonts/Courier Prime Sans.ttf")
-    let fontLen: int = len(font)
-
-    var output = """
-import raylib
-
-var fontArray: array["""
-    add(output, $fontLen)
-    add(output, ", char] = [")
-
-    let startLen = len(output)
-
-    for c in font:
-        addSep(output, startLen = startLen)
-        add(output, "'\\x")
-        add(output, toHex(uint8(c)))
-        add(output, "'")
-        
-
-    add(output, "]\n")
-
-    add(output, """
-
-var fontBytes: ptr char = cast[ptr char](addr(fontArray))
-
-var loadedFont*: Font = LoadFontFromMemory(
-    "ttf",
-    fontBytes,
-    """)
-    add(output, $fontLen)
-    add(output, """,
-    32,
-    nil,
-    95
-)
-""")
-
-    add(output, "var license* = \"\"\"\n")
-    let license: string = readFile("../fonts/LICENSE.md")
-    add(output, license)
-    add(output, "\n\"\"\"")
-
-    writeFile("../font.nim", output)
-# }
+writeFile("../spritesheet.nim", output)
