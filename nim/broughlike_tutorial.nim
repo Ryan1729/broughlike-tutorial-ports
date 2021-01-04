74994d15d3bf352432967e8d4dc294fcda776587
Ryan1729 <Ryan1729@gmail.com>
2021-01-04 00:06:00 -0700

    make a version that has a clearer motivation, but still causes the c compile error

--- nim/broughlike_tutorial.nim
+++ nim/broughlike_tutorial.nim
@@ -1,19 +1,27 @@
+import options
+
 type
-    Spell = proc(state: var bool)
+    Player = bool
+    State = tuple
+        playerOption: Option[Player]
+
+    Spell = proc(state: var State)
 
 proc requirePlayer(
-    spellMaker: proc(field: bool): Spell,
+    spellMaker: proc(player: Player): Spell,
 ): Spell =
-    return proc(state: var bool) =
-        (spellMaker(state))(state)
+    return proc(state: var State) =
+        if state.playerOption.isSome:
+            (spellMaker(state.playerOption.get))(state)
 
 const spell*: Spell =
     requirePlayer(
-        proc(field: bool): Spell =
-        return proc(state: var bool) =
+        proc(player: Player): Spell =
+        return proc(state: var State) =
+            # somthing that relies on Player here
             discard
     )
 
-var state = true
+var state = (playerOption: some(true))
 
 spell(state)
