875a7ade3065fe3306043c9876ee828e9182ef73
Ryan1729 <Ryan1729@gmail.com>
2021-01-22 00:06:00 -0700

    copy in an xorshift implementation from my own other code
    See https://github.com/Ryan1729/paper-mario-randomizer

--- rust/libs/state/src/state.rs
+++ rust/libs/state/src/state.rs
@@ -1,6 +1,26 @@
 #![no_std]
 #![deny(clippy::float_arithmetic)]
 
+type Xs = [core::num::Wrapping<u32>; 4];
+
+fn xorshift(xs: &mut Xs) -> u32 {
+    let mut t = xs[3];
+
+    xs[3] = xs[2];
+    xs[2] = xs[1];
+    xs[1] = xs[0];
+
+    t ^= t << 11;
+    t ^= t >> 8;
+    xs[0] = t ^ xs[0] ^ (xs[0] >> 19);
+
+    xs[0].0
+}
+
+fn xs_u32(xs: &mut Xs, min: u32, max: u32) -> u32 {
+    (xorshift(xs) % (max - min)) + min
+}
+
 pub type TileCount = u8;
 
 pub const NUM_TILES: TileCount = 9;
