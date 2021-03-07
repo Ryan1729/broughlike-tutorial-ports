a94cc616bde38f698d788fc48a4b2fb95b1d59cf
Ryan1729 <Ryan1729@gmail.com>
2021-03-07 00:06:00 -0700

    bake the image into the binary!

--- c/pack/pack.c
+++ c/pack/pack.c
@@ -7,10 +7,34 @@ int main(void) {
         return 1;
     }
 
-    int image_length = 1;
+    FILE* input = fopen("../assets/spritesheet.png", "rb");
 
-    fprintf(output, "static const unsigned char SPRITESHEET_BYTES[%d] = {0};\n", image_length);
-    fprintf(output, "static const int SPRITESHEET_LENGTH = %d;\n", image_length);
+    if (!input) {
+        return 2;
+    }
+
+    if (fseek(input, 0, SEEK_END) != 0) {
+        return 3;
+    }
+
+    long image_length = ftell(input);
+    if (image_length < 0) {
+        return 4;
+    }
+
+    fseek(input, 0, SEEK_SET);
+
+    fprintf(output, "static const unsigned char SPRITESHEET_BYTES[%ld] = {", image_length);
+
+    char* sep = "";
+    int c; // `int`, not char, is required to handle EOF
+    while ((c = fgetc(input)) != EOF) {
+        fprintf(output, "%s%u", sep, c);
+        sep = ",";
+    }
+
+    fprintf(output, "};\n");
+    fprintf(output, "static const int SPRITESHEET_LENGTH = %ld;\n", image_length);
 
     return 0;
 }
