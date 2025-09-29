29adc143a72c685774a15e9f0ba519ed8a7680a2
Ryan1729 <Ryan1729@gmail.com>
2025-09-29 01:06:00 -0600

    Display Hello World in black on white

--- python/broughlike_tutorial.py
+++ python/broughlike_tutorial.py
@@ -1,1 +1,35 @@
+# Example file showing a circle moving on screen
+import pygame
 
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+font = pygame.font.SysFont(pygame.font.get_fonts()[0], 30)
+
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("white")
+
+    text_surface = font.render('Hello, World!', True, "black")
+
+    screen.blit(text_surface, (0, 0))
+
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
